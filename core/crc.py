b08474d9b4e3750af4c366bdce5e53c500e2d555
Mohamed Oussema Boukadida <141274355+Oussema09BKD@users.noreply.github.com>
2026-03-25 18:44:07 +0100

    Implement CRC algorithm with XOR division

--- core/crc.py
+++ core/crc.py
@@ -1,18 +1,42 @@
+data = "100100"
+generator = "1101"
 
-x = input("Please enter the first data block")
-y = input("Please enter the second data block")
-def xor(x,y):
- result=""
- if len(x) == len(y):
-# x = 1001 ,  y = 1010 
-# x XOR y = 0011
-    for i in range (len(x)):
-     if x[i] == y[i] :
-      result = result + "0"
-     else: 
-      result = result + "1"
- else:
-  print("The two data blocks must have equal lengths")
- return result
+# XOR function
+def xor(x, y):
+    result = ""
+    for i in range(len(x)):
+        result += "0" if x[i] == y[i] else "1"
+    return result
+# Appending "L-1" bits to the original data block
+def extended_data(data, generator):
+    return data + "0" * (len(generator) - 1)
 
-print(f"{x} xor {y} = ",xor(x,y))
+# Generating the remainder 
+def compute_crc_remainder(data, generator):
+    n = len(generator)
+    
+    new_data = extended_data(data, generator)
+    window = new_data[:n]
+
+    for i in range(len(data)):
+        # Step 1: XOR
+        if window[0] == "1":
+            window = xor(window, generator)
+        else:
+            window = xor(window, "0" * n)
+        
+        # Step 2: shift + bring next bit
+        if i + n < len(new_data):
+            window = window[1:] + new_data[i + n]
+        else:
+            window = window[1:]
+    
+    return window
+
+def final_crc_data_block(data, generator):
+    return data + compute_crc_remainder(data, generator)
+
+
+print("Extended:", extended_data(data, generator))
+print("Remainder:", compute_crc_remainder(data, generator))
+print("Final crc data block:", final_crc_data_block(data, generator))
